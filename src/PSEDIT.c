f6b8fb2d96c0ba349b18e612233ec6b928f6f65c
Mike Pontillo <mpontillo@mbp2011.local>
2015-05-31 23:52:15 -0700

    Initial commit.
     * This commit consists of all the files I found on
       an old backup disk.
    
     * This is the first software I ever released, so
       I'm sure it doesn't follow the DRY principle!

---
+++ src/PSEDIT.c
@@ -0,0 +1,88 @@
+/* Output from p2c 1.21alpha-07.Dec.93, the Pascal-to-C translator */
+/* From input file "PSEDIT.PAS" */
+
+
+#include <p2c/p2c.h>
+/* p2c: PSEDIT.PAS, line 1: Warning: Could not find module MYIO [271] */
+
+
+#include "myio.h"
+/* p2c: PSEDIT.PAS, line 1: Warning: Could not find module WINDOWS [271] */
+#include "windows.h"
+/* p2c: PSEDIT.PAS, line 1: 
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 2: 
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 3: 
+ * Warning: Could not open include file psedit.inc [230] */
+/* p2c: PSEDIT.PAS, line 8: 
+ * Warning: Unrecognized character 015 in file [247] */
+
+/*
+
+
+
+const
+
+ ver = '.50/alpha';
+
+
+
+ n_attr        = $17; {normal text attribute*/
+main(argc, argv)
+int argc;
+Char *argv[];
+{   /*status line attribute*/
+/* p2c: PSEDIT.PAS, line 9:
+ * Warning: Expected BEGIN, found 'status_attr' [227] */
+/* p2c: PSEDIT.PAS, line 9:
+ * Warning: Unrecognized character 015 in file [247] */
+  /*color for input fields*/
+/* p2c: PSEDIT.PAS, line 10:
+ * Warning: Unrecognized character 015 in file [247] */
+  /*title line color*/
+/* p2c: PSEDIT.PAS, line 11:
+ * Warning: Unrecognized character 015 in file [247] */
+  /*lightbar color*/
+/* p2c: PSEDIT.PAS, line 12:
+ * Warning: Unrecognized character 015 in file [247] */
+  /*important stuff*/
+  PASCAL_MAIN(argc, argv);
+/* p2c: PSEDIT.PAS, line 13:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 14:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 15:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 16:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 17:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 18:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 19:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 20:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 21:
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 22:
+ * Warning: Unrecognized character 015 in file [247] */
+  for (j = 1; j <= i; j++)
+    putchar('\n');
+/* p2c: PSEDIT.PAS, line 23: Warning: Symbol 'J' is not defined [221] */
+/* p2c: PSEDIT.PAS, line 23: Warning: Symbol 'I' is not defined [221] */
+/* p2c: PSEDIT.PAS, line 23:
+ * Warning: Unrecognized character 015 in file [247] */
+  exit(EXIT_SUCCESS);
+}
+/* p2c: PSEDIT.PAS, line 24: 
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 25: 
+ * Warning: Unrecognized character 015 in file [247] */
+/* p2c: PSEDIT.PAS, line 26: 
+ * Warning: Junk at end of input file ignored [277] */
+
+
+
+/* End. */
